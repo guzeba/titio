14cd116a2bdf8d21b9453e22a0d6f73426ee24e7
guzeba <142330876+guzeba@users.noreply.github.com>
2024-11-25 11:05:41 -0300

    Add files via upload

---
+++ JOÂO FU 2A 3TRIMESTRE/perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Futebol',
+    'qual foi o primeiro país a ganhar uma copa do mundo?',
+    'Uruguai'
+)
+
+
+criaCartao(
+    'Futebol',
+    ' Qual o clube com mais títulos da Champions League?',
+    'Real Madrid'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Qual time é o maior campeão do Brasileirão Série A?',
+    'Palmeiras'
+)
+
+
+criaCartao(
+    'Futebol',
+    ' Qual seleção ganhou mais Copas do Mundo?',
+    'Brasil'
+)
+
+criaCartao(
+    'Futebol',
+    'qual time é o maior campeão da Copa do Brasil?',
+    'Cruzeiro'
+)
+
+
+criaCartao(
+    'Futebol',
+    'quais times nunca foram rebaixados?',
+    'Flamengo, São Paulo e Cuiába'
+)
+
+
+criaCartao(
+    'futebol',
+    'quantas bolas de ouro tem cristiano Ronaldo?',
+    '5'
+)
+
+
+criaCartao(
+    'Futebol',
+    'qual foi o último brasileiro a ganhar uma bola de ouro?',
+    'Káka'
+)
+
+
+
+
+
+
